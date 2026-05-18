6758100a680d3f7190580a3a361d422e2d3b971e
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-05-18 22:34:28 +0600

    basic function stucture

---
+++ function.py
@@ -0,0 +1,9 @@
+# public class main(
+#     public static void main(string args[]){
+#         # System.out.println("hello world")
+#     }
+# )
+# print("hello world")
+# function stucture
+def  function_name ():
+    # function body
