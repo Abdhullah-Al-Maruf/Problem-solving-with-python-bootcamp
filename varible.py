49099b8bf1c38a9c5575564bb0befc20423682f2
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-05-18 22:27:59 +0600

    first commit

---
+++ varible.py
@@ -0,0 +1,66 @@
+# f string 
+
+
+# number=int(input("enter your number"))
+# number2=int(input("enter your number 2"))
+
+# sum=number+number2 
+
+# print(f"{number} +{number2} =",sum)
+
+
+# a = 10
+# b = 5
+# temp=0;
+# temp = a  # a এর মান temp এ রাখলাম
+# a = b     # b এর মান a তে দিলাম
+# b = temp  # temp (আগের a) এর মান b তে দিলাম
+
+# print(f"After swap: a = {a}, b = {b}")
+
+
+# birayni
+# problem:  we have 500 tk budget now we will  check what can we brought
+# price menu
+
+# pulao_price=190
+# kacchi=400
+# borhani=200
+
+# budget=300;
+
+# if(budget>=borhani):
+#    print("you can buy borhani")
+#  elif(conditon)
+# else:
+#    print("out of budget")        
+
+
+
+# even means = jor
+# odd means =bijoer
+
+# number=int(input("enter your number\n"))
+
+# equation= number % 2 
+# # if(number%2==0)
+
+# if(equation==0):
+#     print(f"{number} is even")
+
+# else:
+#     print(f"{number} is odd") 
+
+
+
+number=45
+num=22
+nu=77
+
+print(number,num,nu, "ata akta bijore shonka")
+print(f"{num} {number} {nu} ata akta jor shonka")
+
+
+
+
+
