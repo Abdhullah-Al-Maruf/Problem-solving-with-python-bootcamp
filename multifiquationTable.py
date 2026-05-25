8d5f0c64ce56e1f45d54ce7d59608737bac1bfb7
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-05-25 22:49:09 +0600

    multifiqation table with for loop

---
+++ multifiquationTable.py
@@ -0,0 +1,16 @@
+# multifiquation table
+
+n =int(input("enter the number for the table"))
+# text formatting 
+print(f"Multifiqation table for {number}")
+
+for i in range(1,11):
+    print(f"{number} x {i} = {number+i}" )
+    print((number ),"x", i ,"=",(number*i))
+ 
+
+# 2 x 1 =2
+# 2 x 2=4
+
+
+
